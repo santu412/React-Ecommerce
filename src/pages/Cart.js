a34bf0baef645849a72ca855ce22966e448d5ae2
SantoshSivaKumar.Govindu <santoshsivakumar.govindu@virginmediao2.co.uk>
2023-04-21 12:29:05 +0100

    initial commit

---
+++ src/pages/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import CartItem from "../components/Cart/CartItem";
+import EmptyCart from "../components/Cart/EmptyCart";
+import { Link } from "react-router-dom";
+import { CartContext } from "../context/cart";
+import { UserContext } from "../context/user";
+export default function CartList() {
+  const { cart, total } = React.useContext(CartContext);
+  const { user } = React.useContext(UserContext);
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
+  return (
+    <section className="cart-items section">
+      <h2>your cart</h2>
+      {cart.map(item => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <h2>total : ${total}</h2>
+      {user.token ? (
+        <Link to="/login" className="btn btn-primary btn-block">
+        login
+      </Link>
+      ) : (
+        <Link to="/checkout" className="btn btn-primary btn-block">
+        checkout1
+      </Link>
+       
+      )}
+    </section>
+  );
+}
---
+++ src/utils/URL.js
@@ -0,0 +1,1 @@
+export default "https://course-api.com/react-store-products";
